288517efcf269f2675c397a9c5c849ab6316b682
Agustín Ammazzagatti <aammazzagatti@IMA-CBA.COM.AR>
2023-04-02 13:11:33 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function to_index() {
+  location.href = "./index.html";
+}
+
+function to_contact() {
+  location.href = "./contacto.html";
+}
+
+function to_certification() {
+  location.href = "./tabla.html";
+}
